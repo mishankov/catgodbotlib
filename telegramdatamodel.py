e4006307dee161682d4ed2ac2922e72d9fb5537d
Denis Mishankov <mishankov@mail.com>
2017-05-06 20:30:16 +0300

    proper import types

--- telegramdatamodel.py
+++ telegramdatamodel.py
@@ -1,3 +1,6 @@
-import user
-import chat
-import messageentity
+from user import User
+from chat import Chat
+from message import Message
+from messageentity import MessageEntity
+from photosize import PhotoSize
+from update import Update
